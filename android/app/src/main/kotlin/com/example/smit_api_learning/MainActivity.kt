6cca73dd4e05822f2d8c89741b3b7f8c28db8725
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-20 00:42:11 +0500

    Api learning

---
+++ android/app/src/main/kotlin/com/example/smit_api_learning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smit_api_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homeview extends StatefulWidget {
+  const Homeview({super.key});
+
+  @override
+  State<Homeview> createState() => _HomeviewState();
+}
+
+class _HomeviewState extends State<Homeview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Learning'),
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
