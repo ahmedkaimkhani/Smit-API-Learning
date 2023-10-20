e349a0de68ccba9070c11ba31ed8ae2d1711bb95
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-21 03:28:22 +0500

    Api

--- lib/screens/without model user api/user_api.dart
+++ lib/screens/without model user api/user_api.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
 
 class UserApi extends StatefulWidget {
   const UserApi({super.key});
@@ -8,8 +11,22 @@ class UserApi extends StatefulWidget {
 }
 
 class _UserApiState extends State<UserApi> {
+  getPosts() async {
+    var url = Uri.https('jsonplaceholder.typicode.com', '/users');
+
+    var response = await http.get(url);
+
+    print(response);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getPosts();
+        },
+      ),
+    );
   }
 }
