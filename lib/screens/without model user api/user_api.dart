4ee2474c45cbcc226e317ef21689e8357d32cd63
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-21 03:01:54 +0500

    Api

---
+++ lib/screens/without model user api/user_api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserApi extends StatefulWidget {
+  const UserApi({super.key});
+
+  @override
+  State<UserApi> createState() => _UserApiState();
+}
+
+class _UserApiState extends State<UserApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
