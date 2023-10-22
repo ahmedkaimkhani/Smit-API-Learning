29b0f06b30b7c5134cb89ecd127915fc2f9dbf1a
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-23 04:25:36 +0500

    Api

---
+++ lib/screens/user api/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme =
+    ThemeData(primaryColor: Colors.blue, brightness: Brightness.light);
+
+final ThemeData darkTheme =
+    ThemeData(primaryColor: Colors.blue, brightness: Brightness.dark);
