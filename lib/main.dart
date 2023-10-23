4fa2d8be9e1e29ed8371c9ec68c07cd10c2e2ba2
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-23 05:50:33 +0500

    Api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smit_api_learning/screens/user%20api/theme.dart';
 import 'package:smit_api_learning/screens/user%20api/user_api.dart';
-import 'package:smit_api_learning/screens/without%20model%20user%20api/user_api.dart';
 
 bool isDarkMode = false;
 
@@ -16,13 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        key: UniqueKey(),
         title: 'Api Learning',
         debugShowCheckedModeBanner: false,
-        theme: isDarkMode ? lightTheme : darkTheme,
+        theme: isDarkMode ? darkTheme : lightTheme,
         // theme: ThemeData(
         //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         //   useMaterial3: true,
         // ),
-        home: UserApi());
+        home: const UserApi());
   }
 }
--- lib/screens/user api/user_api.dart
+++ lib/screens/user api/user_api.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:smit_api_learning/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:smit_api_learning/services/user_model.dart';
 
 class UserApi extends StatefulWidget {
   const UserApi({super.key});
@@ -15,24 +19,58 @@ class _UserApiState extends State<UserApi> {
     });
   }
 
+  Future<List<UserModel>> getUserData() async {
+    List<UserModel> allUsers = [];
+    var url = Uri.https('jsonplaceholder.typicode.com', '/users');
+    var response = await http.get(url);
+    var responseBody = jsonDecode(response.body);
+    for (var i in responseBody) {
+      allUsers.add(UserModel.fromJson(i));
+    }
+    return allUsers;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              Text('Fetching Api Data'),
-              Switch(
-                value: isDarkMode,
-                onChanged: (value) {
-                  toggleTheme();
-                },
-              )
-            ],
-          ),
-        ],
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Fetching Api Data',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                ),
+                Switch(
+                  inactiveThumbColor: Colors.black,
+                  value: isDarkMode,
+                  onChanged: (value) {
+                    print(isDarkMode);
+                    toggleTheme();
+                  },
+                )
+              ],
+            ),
+            FutureBuilder(
+                future: getUserData(),
+                builder: ((BuildContext context,
+                    AsyncSnapshot<List<UserModel>> snapshot) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile();
+                    },
+                  );
+                })),
+          ],
+        ),
       )),
     );
   }
